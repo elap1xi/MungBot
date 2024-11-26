8cd43678573c03059a4bd2da94353efd8058ec6b
elap1xi <gangsangwon40@gmail.com>
2024-11-26 18:29:01 +0900

    edit file

--- app_command/encryption.mjs
+++ app_command/encryption.mjs
@@ -8,8 +8,8 @@ const algorithm = 'aes-256-cbc';
 const key = crypto.randomBytes(32);
 const iv = crypto.randomBytes(16);
 
-fs.writeFileSync('secret.key', key);
-fs.writeFileSync('iv.key', iv);
+fs.writeFileSync('./data/security/secret.key', key);
+fs.writeFileSync('./data/security/iv.key', iv);
 
 const data = fs.readFileSync('./data/config/config.json');
 const cipher = crypto.createCipheriv(algorithm, key, iv);
