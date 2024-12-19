be5e419e6c3d958a26ddf3444a85302a8f5ed84a
elap1xi <gangsangwon40@gmail.com>
2024-12-19 22:35:04 +0900

    beta branch test

--- events/interactionCreate.mjs
+++ events/interactionCreate.mjs
@@ -58,6 +58,9 @@ async function execute(interaction) {
                     highWaterMark: 1 << 25,
                     liveBuffer: 1 << 25,
                     requestOptions: {
+                        headers: {
+                            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+                        },
                         maxRedirections: 5
                     }
                 });
