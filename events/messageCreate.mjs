41d9595fccc5f48f4f4e6ce08eabf1331452f0d6
elap1xi <gangsangwon40@gmail.com>
2025-01-05 18:48:11 +0900

    ? what

--- events/messageCreate.mjs
+++ events/messageCreate.mjs
@@ -5,7 +5,7 @@ const name = 'messageCreate';
 
 var silencio = false;
 var stupefy = false;
-async function execute(message) {896317141329006622
+async function execute(message) {
     if (message.author.bot && message.author.id !== "1014813681734918144" && message.author.id !== "896317141329006622") return;
 
     const client = message.client;
