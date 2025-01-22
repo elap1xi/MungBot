d6990e175aaaf534fa4150dc1a5582d680f24063
elap1xi <gangsangwon40@gmail.com>
2025-01-22 09:44:17 +0900

    log time : utc+9

--- events/voiceStateUpdate.mjs
+++ events/voiceStateUpdate.mjs
@@ -6,15 +6,17 @@ async function execute(oldState, newState) {
     if(config.status == "test") return;
     if(newState.guild.id !== "1226196255605002292") return;
 
+    let date = new Date();
+    let dateOption = { timeZone: 'Asia/Seoul', hour12: false };
 	if (!oldState.channel && newState.channel) {
         webhookclient_voice_k.send({
-            content: `${newState.member.user.tag} joined voice channel : ${newState.channel.name} - ${Date.now()}`
+            content: `> \`\`${newState.member.user.tag}\`\` joined voice channel : **${newState.channel.name}** _ ${date.toLocaleString('ko-KR', dateOption)}`
         });
     }
     
     if (oldState.channel && !newState.channel) {
         webhookclient_voice_k.send({
-            content: `${oldState.member.user.tag} left voice channel : ${oldState.channel.name} - ${Date.now()}`
+            content: `> \`\`${oldState.member.user.tag}\`\` left voice channel : **${oldState.channel.name}** _ ${date.toLocaleString('ko-KR', dateOption)}`
         });
     }
 }
